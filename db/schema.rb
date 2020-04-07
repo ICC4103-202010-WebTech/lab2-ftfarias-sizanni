1574792cae073ee8b9cf7636089763a71df40140
Sebastian <sizanni@miuandes.cl>
2020-04-06 20:29:26 -0400

    Final commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_022020) do
+ActiveRecord::Schema.define(version: 2020_04_06_235111) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_022020) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "event_id"
+    t.string "category"
     t.index ["event_id"], name: "index_tickets_on_event_id"
   end
 
